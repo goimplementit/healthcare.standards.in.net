7832a026ec546ec3215171b0771ad28ce8162a06
jkv <jenskristianvilladsen@gmail.com>
2017-02-17 21:32:50 +0100

    Added XDS generated stuff

---
+++ IHE.XDS.DK/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHE.XDS.DK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new WebService.DocumentRepository_PortTypeClient();
+        }
+    }
+}
